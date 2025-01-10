4e4997fb6a28b91d477515af3a4f80cba2bd7249
Assaf Klein <assafklein@campus.technion.ac.il>
2025-01-10 17:15:55 +0200

    versions

--- MapRed/Runfiles/MR_Inmem_16G_Copt.sh
+++ MapRed/Runfiles/MR_Inmem_16G_Copt.sh
@@ -25,7 +25,6 @@ fi
 # Save the dir provided as the second argument in a variable
 export OUTPUT_DIR="$2"
 
-cd /home/assaf/Repos/form
 
 # Start HDFS and YARN (if not already running)
 #stop-dfs.sh
@@ -58,18 +57,17 @@ if [ -d "$OUTPUT_DIR" ]; then
     rm -r "$OUTPUT_DIR"
 fi
 
-mkdir -p $OUTPUT_DIR
+sudo mkdir -p $OUTPUT_DIR
 
 # Run the WordCount job
-time hadoop jar /home/assaf/Repos/form/MapRed/Hadoop/fraction_num_term/ComplexTermProcessing.jar ComplexTermFractionDriver \
+time hadoop jar ~/form/MapRed/Hadoop/fraction_num_term/ComplexTermProcessing.jar ComplexTermFractionDriver \
     -D mapreduce.task.io.file.buffer.size=524288  \
-    -D mapreduce.map.memory.mb=1536 \
-    -D mapreduce.map.java.opts=-Xmx1024m\
-    -D mapreduce.task.io.sort.mb=576 \
-    -D mapreduce.map.sort.spill.percent=0.95 \
+    -D mapreduce.map.memory.mb=3072 \
+    -D mapreduce.map.java.opts=-Xmx2048m\
+    -D mapreduce.task.io.sort.mb=1280 \
     -D mapreduce.task.io.sort.factor=100 \
-    -D mapreduce.reduce.memory.mb=2400 \
-    -D mapreduce.reduce.java.opts=-Xmx1736m \
+    -D mapreduce.reduce.memory.mb=3072 \
+    -D mapreduce.reduce.java.opts=-Xmx2400m \
     -D mapreduce.map.output.compress=true \
     -D mapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.SnappyCodec \
     -D mapreduce.reduce.shuffle.input.buffer.percent=0.9 \
@@ -78,33 +76,15 @@ time hadoop jar /home/assaf/Repos/form/MapRed/Hadoop/fraction_num_term/ComplexTe
     -D mapreduce.map.speculative=false \
     -D mapreduce.job.reduces=2\
     -D mapreduce.job.maps=2 \
+    -D mapreduce.tasktracker.map.tasks.maximum=1 \
+    -D mapreduce.job.running.map.limit=2\
     -D mapreduce.reduce.speculative=false \
-    -D mapreduce.input.fileinputformat.split.minsize=536870912\
+    -D mapreduce.input.fileinputformat.split.minsize=1073741824\
     $DFS_INPUT_DIR $DFS_OUTPUT_DIR #> $OUTPUT_DIR/log.txt 2>&1
-    #-D mapreduce.reduce.shuffle.parallelcopies = 8 \
-    #-D mapred.job.reduce.input.buffer.percent=0.7\
-    #-D mapreduce.reduce.shuffle.memory.limit.percent=1.0\
-    #-D mapreduce.input.fileinputformat.split.maxsize=67108864 \
-    #-D mapreduce.input.fileinputformat.split.minsize=33554432 \
-    #-D mapreduce.tasktracker.reduce.tasks.maximum=6 \
-    #-D mapreduce.tasktracker.map.tasks.maximum=6 \
-    #-D mapreduce.task.profile=true \
-    #-D mapreduce.task.profile.maps=0 \
-    #-D mapreduce.task.profile.reduces=0 \
-    #-D mapreduce.task.profile.params="-agentlib:hprof=cpu=samples,heap=sites,depth=6" \
-    #-D mapreduce.input.fileinputformat.split.minsize=268435456 \
-
- #&> $OUTPUT_DIR/log.txt
-    #-Dmapreduce.map.memory.mb=2048 \
-  #  -D mapreduce.job.reduces = 1 \ 
-  #  -D mapreduce.map.output.compress=true \
-  #  -D mapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.SnappyCodec \
-  #  -D mapreduce.task.io.sort.mb = 512 \
      
 
 # Copy the output from HDFS to the local filesystem
-
-#hdfs dfs -get $DFS_OUTPUT_DIR/* $OUTPUT_DIR/
+hdfs dfs -get $DFS_OUTPUT_DIR/* $OUTPUT_DIR/
 
 # Optionally stop Hadoop services after job completion
 #stop-dfs.sh
--- tests/No_combining/sort_from_lowfirst_16G_io.frm
+++ tests/No_combining/sort_from_lowfirst_16G_io.frm
@@ -3,10 +3,14 @@ format 255;
 on powerfirst;
 Symbols a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
 
-Local F = (a+b+c+j+k)^10 * (f+e+g+h)^10 * (i+j+k)^10 * (n+m)^10;
+Local F = (a+b+c+d)^10 * (f+e+g+h)^10 * (i+j+k)^10 * l^10;
 
 
 .Sort
+id d = j + k; 
+.sort
+id l = n + m;
+.sort
 id f = o + p;
 .sort
 off powerfirst;
